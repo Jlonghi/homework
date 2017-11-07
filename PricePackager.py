6b6e2287ba2cfbfc02ebba4220842059cf752e89
josh0588 <joshualonghi@gmail.com>
2017-11-06 22:41:27 -0500

    added person markup

--- PricePackager.py
+++ PricePackager.py
@@ -1,5 +1,8 @@
 def pricePackager(price, people, itemtype):
     flatMarkupTotal = price*1.05
+
+    personMarkup = flatMarkupTotal * (int(people[0])*0.012)
     #debug
-    print(flatMarkupTotal)
+    print('flatMarkupTotal: ', flatMarkupTotal)
+    print('personMarkup: ', personMarkup)
 pricePackager(1299.99, '3 people', 'food')
